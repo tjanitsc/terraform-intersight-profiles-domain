0b9b50f8445c7c9fa95cfa16efa38832b8b0bd3d
scotttyso <tyscott@cisco.com>
2022-09-25 03:18:38 -0400

    adding A and B Switches to outputs

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,12 @@
 # Collect the moid of the UCS Domain Cluster Profile
 #____________________________________________________________
 
-output "moid" {
+output "domain_profile" {
   description = "UCS Domain Cluster Profile Managed Object ID (moid)."
-  value       = intersight_fabric_switch_cluster_profile.domain_profile.moid
+  value = {
+    name = intersight_fabric_switch_cluster_profile.domain_profile.name
+    moid = intersight_fabric_switch_cluster_profile.domain_profile.moid
+    A    = intersight_fabric_switch_profile.switch_profiles["A"].moid
+    B    = intersight_fabric_switch_profile.switch_profiles["B"].moid
+  }
 }
