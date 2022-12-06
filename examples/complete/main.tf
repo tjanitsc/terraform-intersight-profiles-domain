aa897bc63712d40cf7d0c4f5eed7d094278aaeba
tjanitsc <37362339+tjanitsc@users.noreply.github.com>
2022-12-06 23:36:37 +0100

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -10,59 +10,7 @@ resource "intersight_server_profile" "server1" {
   }
 }
 
-resource "intersight_server_profile" "server2" {
-  name   = "tf_server2"
-  action = "No-op"
-  tags {
-    key   = "server"
-    value = "demo"
-  }
-  organization {
-    object_type = "organization.Organization"
-    moid        = var.organization
-  }
-}
-
-resource "intersight_server_profile" "server3" {
-  name   = "tf_server3"
-  action = "No-op"
-  tags {
-    key   = "server"
-    value = "demo"
-  }
-  organization {
-    object_type = "organization.Organization"
-    moid        = var.organization
-  }
-}
-
-resource "intersight_server_profile" "server4" {
-  name   = "tf_server4"
-  action = "No-op"
-  tags {
-    key   = "server"
-    value = "demo"
-  }
-  organization {
-    object_type = "organization.Organization"
-    moid        = var.organization
-  }
-}
-
-resource "intersight_server_profile" "server5" {
-  name   = "tf_server5"
-  action = "No-op"
-  tags {
-    key   = "server"
-    value = "demo"
-  }
-  organization {
-    object_type = "organization.Organization"
-    moid        = var.organization
-  }
-}
-
-variable "organization" {
-   type = string
-   description = "<value for organization>"
-}
+#variable "organization" {
+#   type = string
+#   description = "<value for organization>"
+#}
