406aae8c75e6a09efb4609a3d723fcfda64a206d
tjanitsc <37362339+tjanitsc@users.noreply.github.com>
2022-12-07 00:07:15 +0100

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,14 +1,6 @@
 resource "intersight_server_profile" "server" {
   name   = "tf_server"
   action = "No-op"
-  tags {
-    key   = "server"
-    value = "demo"
-  }
-  organization {
-    object_type = "default"
-  }
-  
   target_platform = "Standalone"
 }
 
