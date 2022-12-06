cb180d9d147b3ac1c1986228e68020b11160ac07
tjanitsc <37362339+tjanitsc@users.noreply.github.com>
2022-12-07 00:25:45 +0100

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -7,6 +7,18 @@ resource "intersight_server_profile" "server" {
     moid        = data.intersight_organization_organization.org_data.results[0].moid
   }
 }
+
+data "intersight_organization_organization" "org_data" {
+  name = var.org_name
+}
+
+variable "org_name" {
+  type        = string
+  description = "Name of the Org where you want to create the resource"
+  default     = "default"
+}
+
+
 #variable "organization" {
 #   type = string
 #   description = "<value for organization>"
