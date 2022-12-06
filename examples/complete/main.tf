86f22dbe7f2618586d12ab62c23d94a4bbd80ec7
tjanitsc <37362339+tjanitsc@users.noreply.github.com>
2022-12-06 23:52:48 +0100

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -9,20 +9,8 @@ resource "intersight_server_profile" "server" {
     object_type = "default"
   }
   
-  target_platform = var.sp_target_platform
-  action          = var.sp_action
-}
-
-variable "sp_target_platform" {
-  type        = string
-  description = "The platform for which the server profile is applicable. It can either be a server that is operating in standalone mode or which is attached to a Fabric Interconnect managed by Intersight."
-  default     = "Standalone" # Options: Standalone, FIAttached
-}
-
-variable "sp_action" {
-  type        = string
-  description = "service profile operations"
-  default     = "No-op" # Options: # Deploy, Unassign
+  target_platform = "Standalone"
+  action          = "No-op"
 }
 
 #variable "organization" {
