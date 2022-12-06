3622ce68fd175b8fa0d4673bdbd68af36a820867
tjanitsc <37362339+tjanitsc@users.noreply.github.com>
2022-12-06 23:08:33 +0100

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,5 +1,57 @@
 resource "intersight_server_profile" "server1" {
-  name   = "server1"
+  name   = "tf_server1"
+  action = "No-op"
+  tags {
+    key   = "server"
+    value = "demo"
+  }
+  organization {
+    object_type = "organization.Organization"
+    moid        = var.organization
+  }
+}
+
+resource "intersight_server_profile" "server2" {
+  name   = "tf_server2"
+  action = "No-op"
+  tags {
+    key   = "server"
+    value = "demo"
+  }
+  organization {
+    object_type = "organization.Organization"
+    moid        = var.organization
+  }
+}
+
+resource "intersight_server_profile" "server3" {
+  name   = "tf_server3"
+  action = "No-op"
+  tags {
+    key   = "server"
+    value = "demo"
+  }
+  organization {
+    object_type = "organization.Organization"
+    moid        = var.organization
+  }
+}
+
+resource "intersight_server_profile" "server4" {
+  name   = "tf_server4"
+  action = "No-op"
+  tags {
+    key   = "server"
+    value = "demo"
+  }
+  organization {
+    object_type = "organization.Organization"
+    moid        = var.organization
+  }
+}
+
+resource "intersight_server_profile" "server5" {
+  name   = "tf_server5"
   action = "No-op"
   tags {
     key   = "server"
