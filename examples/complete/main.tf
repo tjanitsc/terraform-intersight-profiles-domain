5bdbb4a097cfc1f9a6d38917b51f958b89ecb2c2
tjanitsc <37362339+tjanitsc@users.noreply.github.com>
2022-12-07 13:01:17 +0100

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -6,6 +6,13 @@ resource "intersight_server_profile" "server1" {
     moid = "638f438e77696e2d3015928a"
     object_type = "server.ProfileTemplate"
   }
+  dynamic "policy_bucket" {
+    for_each = [for s in var.policy_bucket : s if s != null]
+    content {
+      moid        = policy_bucket.value.moid
+      object_type = policy_bucket.value.object_type
+    }
+  }
   organization {
     object_type = "organization.Organization"
     moid        = data.intersight_organization_organization.org_data.results[0].moid
@@ -65,6 +72,79 @@ variable "org_name" {
   description = "Name of the Org where you want to create the resource"
   default     = "default"
 }
+
+policy_bucket = [
+    {
+      # BIOS Policy
+      moid        = module.bios_example.moid
+      object_type = "bios.Policy"
+    },
+    {
+      # Boot Policy
+      moid        = module.boot_uefi_local_disk.moid
+      object_type = "boot.PrecisionPolicy"
+    },
+    {
+      # IPMI over LAN Policy
+      moid        = module.ipmi_example.moid
+      object_type = "ipmioverlan.Policy"
+    },
+    {
+      # IMC Access Policy
+      moid        = module.imc_access_example.moid
+      object_type = "access.Policy"
+    },
+    {
+      # LAN Connectivity Policy
+      moid        = module.lan_connectivity_example.moid
+      object_type = "vnic.LanConnectivityPolicy"
+    },
+    {
+      # Local user Policy
+      moid        = module.local_user_policy_example.moid
+      object_type = "iam.EndPointUserPolicy"
+    },
+    {
+      # SAN Connectivity Policy
+      moid        = module.san_connectivity.moid
+      object_type = "vnic.SanConnectivityPolicy"
+    },
+    {
+      # SD Card Policy
+      moid        = module.sd_card_example.moid
+      object_type = "sdcard.Policy"
+    },
+    {
+      # Serial over LAN Policy
+      moid        = module.sol_example.moid
+      object_type = "sol.Policy"
+    },
+    {
+      # SNMP Policy
+      moid        = module.snmp_example.moid
+      object_type = "snmp.Policy"
+    },
+    # {
+    #   # Storage Policy
+    #   moid        = module.storage_example.moid
+    #   object_type = "storage.StoragePolicy"
+    # },
+    {
+      # Syslog Policy
+      moid        = module.syslog_example.moid
+      object_type = "syslog.Policy"
+    },
+    {
+      # Virtual KVM Policy
+      moid        = module.kvm_example.moid
+      object_type = "kvm.Policy"
+    },
+    {
+      # Virtual Media Policy
+      moid        = module.vmedia_example_1.moid
+      object_type = "vmedia.Policy"
+    },
+  ]
 
 #variable "organization" {
 #   type = string
