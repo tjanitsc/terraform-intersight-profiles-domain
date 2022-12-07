424f76cd293c01a44a1ba8b7294d99f4b6f6343d
tjanitsc <37362339+tjanitsc@users.noreply.github.com>
2022-12-07 12:27:01 +0100

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -10,6 +10,13 @@ resource "intersight_server_profile" "server1" {
     object_type = "organization.Organization"
     moid        = data.intersight_organization_organization.org_data.results[0].moid
   }
+  dynamic "policy_bucket" {
+    for_each = [for s in var.policy_bucket : s if s != null]
+    content {
+      moid        = policy_bucket.value.moid
+      object_type = policy_bucket.value.object_type
+    }
+  }
 }
 
 resource "intersight_server_profile" "server2" {
