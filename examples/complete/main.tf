7a24ca3142447d105f5dea0f62f949024e333996
tjanitsc <37362339+tjanitsc@users.noreply.github.com>
2022-12-07 18:15:10 +0100

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -64,16 +64,11 @@ ressource "intersight_ntp_policy" "ntpterraformtest" {
     moid        = data.intersight_organization_organization.org_data.results[0].moid
   }
   
-  profiles [
-    {
+  profiles {
       moid = intersight_server_profile.server2.moid
       object_type = "server.Profile"
-    },
-    {
-      moid = intersight_server_profile.server3.moid
-      object_type = "server.Profile"
     }
-   ]
+   
 }
 
 
